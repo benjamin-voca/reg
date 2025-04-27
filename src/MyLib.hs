c96494388015e1ca79b9ed97e93e2cdf0a1066c7
benjamin-voca <benjaminvocaa@gmail.com>
2025-04-27 20:17:56 +0200

    adding data-structure

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,4 +1,46 @@
-module MyLib (someFunc) where
+module MyLib (Regex(..), RegexSymbol(..), RClass(..), RClassElem(..), Quant(..),regex, match) where
+regex :: String -> Regex
+regex str = undefined
+match :: Regex -> String -> Bool
+match reg str = undefined
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+-- The main regex tree
+data Regex
+  = Seq [Regex]            -- Sequence of regex elements
+  | Alt [Regex]            -- Alternation (|)
+  | Quantified Regex Quant -- Quantified expression (*, +, ?, {n,m})
+  | Group Regex            -- Grouping (e.g., (abc))
+  | Symbol RegexSymbol     -- Basic symbols like literal, dot, anchors
+  deriving (Show, Eq)
+
+-- Quantifiers for repetitions
+data Quant
+  = ZeroOrMore            -- *
+  | OneOrMore             -- +
+  | ZeroOrOne             -- ?
+  | RangeQuant Int (Maybe Int) -- {n} or {n,} or {n,m}
+  deriving (Show, Eq)
+
+-- Basic building blocks
+data RegexSymbol
+  = Caret                 -- ^
+  | Dollar                -- $
+  | Any                   -- . (dot)
+  | Literal Char          -- A literal character
+  | Escape Char           -- An escaped character (\d, \w, etc.)
+  | Class RClass          -- Character class like [a-z]
+  deriving (Show, Eq)
+
+-- Character class definition
+data RClass = RClass
+  { negated :: Bool              -- True if [^...]
+  , elements :: [RClassElem]      -- List of elements
+  }
+  deriving (Show, Eq)
+
+-- Elements inside a character class
+data RClassElem
+  = Single Char                  -- Single character like 'a'
+  | Range Char Char              -- Range like 'a'-'z'
+  | PosixClass String            -- [:digit:], [:alpha:], etc.
+  deriving (Show, Eq)
--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,53 @@
+-- test/RegexTest.hs
 module Main (main) where
 
+import Test.Tasty (defaultMain, testGroup, TestTree)
+import Test.Tasty.HUnit (testCase, (@?=))
+import qualified Data.Text as T
+
+import MyLib (regex, match)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Regex Tests"
+  [ testCase "Simple exact match" $
+      match (regex "hello") (T.pack "hello") @?= True
+
+  , testCase "Simple mismatch" $
+      match (regex "hello") (T.pack "world") @?= False
+
+  , testCase "Wildcard match" $
+      match (regex "h.llo") (T.pack "hello") @?= True
+
+  , testCase "Multiple matches" $
+      match (regex "foo|bar") (T.pack "bar") @?= True
+
+  , testCase "Start anchor match" $
+      match (regex "^start") (T.pack "start of line") @?= True
+
+  , testCase "Start anchor mismatch" $
+      match (regex "^start") (T.pack "in the start") @?= False
+
+  , testCase "End anchor match" $
+      match (regex "end$") (T.pack "the very end") @?= True
+
+  , testCase "End anchor mismatch" $
+      match (regex "end$") (T.pack "endings are nice") @?= False
+
+  , testCase "Digit matching" $
+      match (regex "[0-9]+") (T.pack "12345") @?= True
+
+  , testCase "No match for digits in text" $
+      match (regex "[0-9]+") (T.pack "abc") @?= False
+
+  , testCase "Empty pattern matches empty text" $
+      match (regex "") (T.pack "") @?= True
+
+  , testCase "Empty pattern matches non-empty text" $
+      match (regex "") (T.pack "something") @?= True
+
+  , testCase "Case sensitive match (fail)" $
+      match (regex "Hello") (T.pack "hello") @?= False
+  ]
